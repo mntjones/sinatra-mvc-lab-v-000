7c17e21e88db51e886070f464e26cc91d41e62ad
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-04 14:47:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,8 +9,12 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
+<<<<<<< HEAD
     @analyzed_text = PigLatinizer.new
     @pig = @analyzed_text.piglatinize(params[:user_phrase])
+=======
+    @analyzed_text = PigLatinizer.piglatinize(params[:user_phrase])
+>>>>>>> 2c9c8dda5cd2d4b980fc9b4ea532842e18ce00b4
     erb :piglatinize
   end
   
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,7 @@ class PigLatinizer
     transform = []
     
     phrase.each do |w|
+<<<<<<< HEAD
 
       latin = w.split /([aeiouAEIOU].*)/
       joy = latin - [""]
@@ -13,6 +14,22 @@ class PigLatinizer
         transform << (joy[1] + joy[0] + "ay")
       else
         transform << (joy[0] + "way")
+=======
+      
+      # if w[0] =~ /[aeiouAEIOU]/
+      #   transform << (w + "way")
+      # elsif w[1] =~ /^[^aeyiuo]+$/
+      #   transform << (w[2..-1] + w[0..1] + "ay")
+      # else
+      #   transform << (w[1..-1] + w[0] + "ay")
+      # end
+      latin = w.split /([aeiou].*)/
+      latin = latin.reject(&:blank?)
+      if latin.length > 1
+        transform << (latin[1] + latin[0] + "ay")
+      else
+        transform << (latin[0] + "way")
+>>>>>>> 2c9c8dda5cd2d4b980fc9b4ea532842e18ce00b4
       end
       
     end
